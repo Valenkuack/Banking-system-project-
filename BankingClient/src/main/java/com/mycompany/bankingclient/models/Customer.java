8eebb4d4fe235530694027fc3e816d1223025ad4
Valeria Hernandez <valenka27@hotmail.com>
2017-04-22 20:52:14 +0100

    update

---
+++ BankingClient/src/main/java/com/mycompany/bankingclient/models/Customer.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bankingclient.models;
+
+import com.mycompany.bankingclient.models.Transaction;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "customer")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
+    , @NamedQuery(name = "Customer.findByCusId", query = "SELECT c FROM Customer c WHERE c.cusId = :cusId")
+    , @NamedQuery(name = "Customer.findByFullName", query = "SELECT c FROM Customer c WHERE c.fullName = :fullName")
+    , @NamedQuery(name = "Customer.findByAddress", query = "SELECT c FROM Customer c WHERE c.address = :address")
+    , @NamedQuery(name = "Customer.findByEmail", query = "SELECT c FROM Customer c WHERE c.email = :email")
+    , @NamedQuery(name = "Customer.findByMobile", query = "SELECT c FROM Customer c WHERE c.mobile = :mobile")
+    , @NamedQuery(name = "Customer.findBySecurityAnswer", query = "SELECT c FROM Customer c WHERE c.securityAnswer = :securityAnswer")
+    , @NamedQuery(name = "Customer.findBySecurityQuestion", query = "SELECT c FROM Customer c WHERE c.securityQuestion = :securityQuestion")
+    , @NamedQuery(name = "Customer.findByPasscode", query = "SELECT c FROM Customer c WHERE c.passcode = :passcode")})
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "cus_id")
+    private Integer cusId;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "mobile")
+    private Integer mobile;
+    @Column(name = "security_answer")
+    private String securityAnswer;
+    @Column(name = "security_question")
+    private String securityQuestion;
+    @Column(name = "passcode")
+    private String passcode;
+    @OneToMany(mappedBy = "cusId")
+    private Collection<Transaction> transactionCollection;
+    @OneToMany(mappedBy = "cusId")
+    private Collection<BankAccount> bankAccountCollection;
+
+    public Customer() {
+    }
+
+    public Customer(Integer cusId) {
+        this.cusId = cusId;
+    }
+
+    public Integer getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(Integer cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Integer mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getSecurityAnswer() {
+        return securityAnswer;
+    }
+
+    public void setSecurityAnswer(String securityAnswer) {
+        this.securityAnswer = securityAnswer;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    @XmlTransient
+    public Collection<Transaction> getTransactionCollection() {
+        return transactionCollection;
+    }
+
+    public void setTransactionCollection(Collection<Transaction> transactionCollection) {
+        this.transactionCollection = transactionCollection;
+    }
+
+    @XmlTransient
+    public Collection<BankAccount> getBankAccountCollection() {
+        return bankAccountCollection;
+    }
+
+    public void setBankAccountCollection(Collection<BankAccount> bankAccountCollection) {
+        this.bankAccountCollection = bankAccountCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cusId != null ? cusId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.cusId == null && other.cusId != null) || (this.cusId != null && !this.cusId.equals(other.cusId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.bankingclient.Customer[ cusId=" + cusId + " ]";
+    }
+    
+}
---
+++ BankingClient/src/main/java/com/mycompany/bankingclient/models/Transaction.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bankingclient.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "transaction")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Transaction_1.findAll", query = "SELECT t FROM Transaction_1 t")
+    , @NamedQuery(name = "Transaction_1.findByTId", query = "SELECT t FROM Transaction_1 t WHERE t.tId = :tId")
+    , @NamedQuery(name = "Transaction_1.findByAmount", query = "SELECT t FROM Transaction_1 t WHERE t.amount = :amount")
+    , @NamedQuery(name = "Transaction_1.findByDescription", query = "SELECT t FROM Transaction_1 t WHERE t.description = :description")
+    , @NamedQuery(name = "Transaction_1.findByTType", query = "SELECT t FROM Transaction_1 t WHERE t.tType = :tType")
+    , @NamedQuery(name = "Transaction_1.findByDate", query = "SELECT t FROM Transaction_1 t WHERE t.date = :date")})
+public class Transaction implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "t_id")
+    private Integer tId;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "amount")
+    private BigDecimal amount;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "t_type")
+    private String tType;
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    @JoinColumn(name = "a_id", referencedColumnName = "a_id")
+    @ManyToOne
+    private BankAccount aId;
+    @JoinColumn(name = "cus_id", referencedColumnName = "cus_id")
+    @ManyToOne
+    private Customer cusId;
+
+    public Transaction() {
+    }
+
+    public Transaction(Integer tId) {
+        this.tId = tId;
+    }
+
+    public Integer getTId() {
+        return tId;
+    }
+
+    public void setTId(Integer tId) {
+        this.tId = tId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTType() {
+        return tType;
+    }
+
+    public void setTType(String tType) {
+        this.tType = tType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BankAccount getAId() {
+        return aId;
+    }
+
+    public void setAId(BankAccount aId) {
+        this.aId = aId;
+    }
+
+    public Customer getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(Customer cusId) {
+        this.cusId = cusId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (tId != null ? tId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Transaction)) {
+            return false;
+        }
+        Transaction other = (Transaction) object;
+        if ((this.tId == null && other.tId != null) || (this.tId != null && !this.tId.equals(other.tId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.bankingclient.Transaction_1[ tId=" + tId + " ]";
+    }
+    
+}
