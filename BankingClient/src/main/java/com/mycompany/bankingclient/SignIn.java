ccdd3b48c1353395e9c2920c9460a40df182f3a8
junjunjuns <junhsin@gmail.com>
2017-04-23 03:55:23 +0800

    Home, NewCustomer, SignIn GUI

--- BankingClient/src/main/java/com/mycompany/bankingclient/SignIn.java
+++ BankingClient/src/main/java/com/mycompany/bankingclient/SignIn.java
@@ -9,12 +9,12 @@ package com.mycompany.bankingclient;
  *
  * @author user
  */
-public class ClientGUI extends javax.swing.JFrame {
+public class SignIn extends javax.swing.JFrame {
 
     /**
-     * Creates new form ClientGUI
+     * Creates new form SignIn
      */
-    public ClientGUI() {
+    public SignIn() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class ClientGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClientGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SignIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ClientGUI().setVisible(true);
+                new SignIn().setVisible(true);
             }
         });
     }
